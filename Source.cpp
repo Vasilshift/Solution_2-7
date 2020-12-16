eb5bc7ec47803ca93d4f2fde26394c654f7215d4
Vasilshift <Vasilshift@github.com>
2020-12-16 13:27:52 +0500

    correct solution

--- Source.cpp
+++ Source.cpp
@@ -12,16 +12,15 @@ int main() {
 	if (a > b) max = b, min = a;
 
 	cout << "Return number(s) between " << a << " and " << b << ":\n";
-	for (i = max; i <= min; i++) 
-		cout << " " << i; 
-		cout << "\n";
+	for (i = max; i <= min; i++) cout << " " << i; 
+	cout << "\n";
 	
 	cout << "Return even number(s):\n";
 	for (i = max; i <= min; i++) if (i % 2 == 0) cout << " " << i;
 	cout << "\n";
 	
 	cout << "Return number(s) multiplicity 7:\n";
-		for (i = a; i <= b; i++) if (i % 7 == 0)  cout << " " << i; 
+	for (i = a; i <= b; i++) if (i % 7 == 0)  cout << " " << i; 
 	cout << "\n";
 		
 	return 0;

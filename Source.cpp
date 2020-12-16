c8b932f09903800f7f13c2265ee37965ec05f67d
Vasilshift <Vasilshift@github.com>
2020-12-16 13:24:04 +0500

    best formatted

--- Source.cpp
+++ Source.cpp
@@ -7,11 +7,11 @@ int main() {
 
 	cout << "Please, enter number ONE and number TWO \n";
 	cin >> a >> b;
-	cout << "Return number(s) between " << a << " and " << b << ":\n";
 	
 	if (a <= b) max = a, min = b;
 	if (a > b) max = b, min = a;
 
+	cout << "Return number(s) between " << a << " and " << b << ":\n";
 	for (i = max; i <= min; i++) 
 		cout << " " << i; 
 		cout << "\n";
@@ -19,9 +19,9 @@ int main() {
 	cout << "Return even number(s):\n";
 	for (i = max; i <= min; i++) if (i % 2 == 0) cout << " " << i;
 	cout << "\n";
+	
 	cout << "Return number(s) multiplicity 7:\n";
-
-	for (i = a; i <= b; i++) if (i % 7 == 0)  cout << " " << i; 
+		for (i = a; i <= b; i++) if (i % 7 == 0)  cout << " " << i; 
 	cout << "\n";
 		
 	return 0;
